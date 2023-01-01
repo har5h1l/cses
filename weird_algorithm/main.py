2893260d125a0cc204faf961f2a134dbe99949de
har5h1l <28hshah@gmail.com>
2023-01-01 13:03:36 -0800

    remove function and edit if/elif/else

--- weird_algorithm/main.py
+++ weird_algorithm/main.py
@@ -1,15 +1,13 @@
-def hailstone(start: int) -> str:
-    sequence = [str(start)]
-    while True:
-        if start % 2 == 0:
-            start = int(start / 2)
-            sequence.append(str(start))
-        elif start == 1:
-            sequence = ' '.join(sequence)
-            return(sequence)
-        else:
-            start = int(3 * start + 1)
-            sequence.append(str(start))
-
 start = int(input())
-print(hailstone(start), end="")
+
+sequence = [str(start)]
+while start != 1:
+    if start % 2 == 0:
+        start = int(start / 2)
+        sequence.append(str(start))
+    else:
+        start = int(3 * start + 1)
+        sequence.append(str(start))
+sequence = ' '.join(sequence)
+
+print(sequence, end="")
