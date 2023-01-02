b19d946ffef3708d7cdbbbdd381b044d38c69cb9
har5h1l <28hshah@gmail.com>
2023-01-01 20:02:07 -0800

    add comment about better alternate solution

--- missing_number/main.py
+++ missing_number/main.py
@@ -9,4 +9,6 @@ for num in nums:
     i = i + 1
 print(n)
 
-# A better and more efficient solution would be to use symmetric.difference with a set from 1 to n and a set that is missing the number, but this solution works fine (it is slower though).
+# A better, more efficient solution would be to use symmetric.difference with a set from 1 to n and a set that is missing the number.
+
+# Another solution (even better) solution would be just to find the difference of the sum of the numbers from 1 to n (use n(n + 1)/2) and the sum of the numbers given (use sum()).
