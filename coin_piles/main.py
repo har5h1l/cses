a66fee2fd2036091bad30ce87cb91ba6d8b11538
har5h1l <28hshah@gmail.com>
2023-08-01 21:54:55 -0700

    complete coin piles

--- coin_piles/main.py
+++ coin_piles/main.py
@@ -2,15 +2,5 @@
 
 for _ in range(int(input())):
     a, b = list(map(int, input().split()))
-    while True:
-        if a > b:
-            a, b = a - 2, b - 1
-        elif b > a:
-            a, b = a - 1, b - 2
-        if a < 0 or b < 0:
-            print('NO')
-            break
-        if a == b:
-            if a % 3 == 0: print('YES')
-            else: print('NO')
-            break
+    if (2 * a - b) >= 0 and (2 * b - a) >= 0 and (2 * a - b) % 3 == 0 and (2 * b - a) % 3 == 0: print('YES')
+    else: print('NO')
