c6fd1b6ab9721f90791e18123a428a3813c87a8c
har5h1l <28hshah@gmail.com>
2023-11-25 22:01:18 -0800

    solve apartments complete

--- sortingandsearching/apartments/main.py
+++ sortingandsearching/apartments/main.py
@@ -1,5 +1,26 @@
 # https://cses.fi/problemset/task/1084
 
 n, m, k = list(map(int, input().split()))
-a = list(map(lambda x: (int(x)-k, int(x)+k), input().split()))
-b = list(map(int, input().split()))
+a = sorted(list(map(int, input().split())))
+b = sorted(list(map(int, input().split())))
+out = 0
+i, j = 0, 0
+
+while i < n:
+    x = a[i]
+    r1, r2 = x - k, x + k
+    while j < m:
+        y = b[j]
+        if y >= r1 and y <= r2:
+            out += 1
+            i += 1
+            j += 1
+            break
+        elif y < r1:
+            j += 1
+        elif y > r2:
+            i += 1
+            break
+    if j >= m: break
+
+print(out)
